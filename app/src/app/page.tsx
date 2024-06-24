6a3eb7f54b22a5ea61aaebbb270039f4eb0c8842
danielred007 <danielrojo1927@gmail.com>
2024-06-24 15:47:23 +0100

    adding support for mongodb

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,14 +1,39 @@
-import Image from "next/image";
+"use client";
+
 import { Header } from "./components/shared/Header";
 import { Footer } from "./components/shared/Footer";
+import { HomeContactForm } from "./components/HomeContactForm";
+import { useEffect, useState } from "react";
+
+type Item = {
+  _id: string;
+  name: string;
+  description: string;
+};
 
 export default function HomePage() {
+  const [items, setItems] = useState<Item[]>([]);
+
+  useEffect(() => {
+    fetch("/api/items")
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.success) {
+          // setItems(data.data);
+          console.log(data);
+        }
+      });
+  }, []);
+
   return (
     <div className="min-h-screen bg-gray-100">
       <Header />
 
       <main>
-        <section id="hero" className="bg-blue-500 text-white py-20">
+        <section
+          id="hero"
+          className="bg-blue-500 dark:bg-blue-900 text-white py-20"
+        >
           <div className="container mx-auto px-4 text-center">
             <h1 className="text-4xl font-bold">Welcome to My Website</h1>
             <p className="mt-4 text-lg">
@@ -20,20 +45,24 @@ export default function HomePage() {
           </div>
         </section>
 
-        <section id="about" className="py-20 bg-white dark:bg-gray-300">
+        <section id="about" className="py-20 bg-white dark:bg-gray-700">
           <div className="container mx-auto px-4 text-center">
-            <h2 className="text-3xl font-bold text-gray-900">About Us</h2>
-            <p className="mt-4 text-gray-900">
+            <h2 className="text-3xl font-bold text-gray-900 dark:text-gray-100">
+              About Us
+            </h2>
+            <p className="mt-4 text-gray-900 dark:text-gray-100">
               We are a team of dedicated professionals committed to delivering
               quality services.
             </p>
           </div>
         </section>
 
-        <section id="services" className="bg-gray-200 py-20">
+        <section id="services" className="bg-gray-200 dark:bg-gray-900 py-20">
           <div className="container mx-auto px-4 text-center">
-            <h2 className="text-3xl font-bold text-gray-800">Our Services</h2>
-            <p className="mt-4 text-gray-600">
+            <h2 className="text-3xl font-bold text-gray-800 dark:text-gray-100">
+              Our Services
+            </h2>
+            <p className="mt-4 text-gray-600 dark:text-white">
               We offer a wide range of services to meet your needs.
             </p>
             <div className="mt-8 flex justify-center space-x-8">
@@ -71,34 +100,7 @@ export default function HomePage() {
             <p className="mt-4 text-gray-600">
               Feel free to reach out to us with any questions or inquiries.
             </p>
-            <form className="mt-8 max-w-md mx-auto">
-              <div className="mb-4">
-                <input
-                  type="text"
-                  placeholder="Your Name"
-                  className="w-full px-4 py-2 border border-gray-300 rounded"
-                />
-              </div>
-              <div className="mb-4">
-                <input
-                  type="email"
-                  placeholder="Your Email"
-                  className="w-full px-4 py-2 border border-gray-300 rounded"
-                />
-              </div>
-              <div className="mb-4">
-                <textarea
-                  placeholder="Your Message"
-                  className="w-full px-4 py-2 border border-gray-300 rounded"
-                ></textarea>
-              </div>
-              <button
-                type="submit"
-                className="px-4 py-2 bg-blue-500 text-white font-semibold rounded hover:bg-blue-600"
-              >
-                Send Message
-              </button>
-            </form>
+            <HomeContactForm />
           </div>
         </section>
       </main>

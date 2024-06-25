61c5398b5a915a7622b79d08b08369dc8231b1fe
danielred007 <danielrojo1927@gmail.com>
2024-06-25 07:25:41 +0100

    adding initial setup for user auth endpoint

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import Link from "next/link";
 import { Header } from "./components/shared/Header";
 import { Footer } from "./components/shared/Footer";
 import { HomeContactForm } from "./components/HomeContactForm";
-import { useEffect, useState } from "react";
+import { HomeTabsSection } from "./components/HomeTabsSection";
 
 type Item = {
   _id: string;
@@ -35,22 +37,22 @@ export default function HomePage() {
           className="bg-blue-500 dark:bg-blue-900 text-white py-20"
         >
           <div className="container mx-auto px-4 text-center">
-            <h1 className="text-4xl font-bold">Welcome to My Website</h1>
+            <h1 className="text-9xl font-bold">X! </h1>
             <p className="mt-4 text-lg">
-              This is a simple hero section to catch your attention.
+              A social media for comments and stuff, Register Now!
             </p>
             <button className="mt-6 px-4 py-2 bg-white text-blue-500 font-semibold rounded hover:bg-gray-200">
-              Learn More
+              <Link href="/about">Learn More</Link>
             </button>
           </div>
         </section>
 
         <section id="about" className="py-20 bg-white dark:bg-gray-700">
           <div className="container mx-auto px-4 text-center">
-            <h2 className="text-3xl font-bold text-gray-900 dark:text-gray-100">
+            <h2 className="text-5xl font-bold text-gray-900 dark:text-gray-100">
               About Us
             </h2>
-            <p className="mt-4 text-gray-900 dark:text-gray-100">
+            <p className="mt-4 text-xl text-gray-900 dark:text-gray-100">
               We are a team of dedicated professionals committed to delivering
               quality services.
             </p>
@@ -65,32 +67,7 @@ export default function HomePage() {
             <p className="mt-4 text-gray-600 dark:text-white">
               We offer a wide range of services to meet your needs.
             </p>
-            <div className="mt-8 flex justify-center space-x-8">
-              <div className="w-1/3 bg-white p-6 rounded shadow-md">
-                <h3 className="text-xl font-semibold text-gray-800">
-                  Service One
-                </h3>
-                <p className="mt-4 text-gray-600">
-                  Description of service one.
-                </p>
-              </div>
-              <div className="w-1/3 bg-white p-6 rounded shadow-md">
-                <h3 className="text-xl font-semibold text-gray-800">
-                  Service Two
-                </h3>
-                <p className="mt-4 text-gray-600">
-                  Description of service two.
-                </p>
-              </div>
-              <div className="w-1/3 bg-white p-6 rounded shadow-md">
-                <h3 className="text-xl font-semibold text-gray-800">
-                  Service Three
-                </h3>
-                <p className="mt-4 text-gray-600">
-                  Description of service three.
-                </p>
-              </div>
-            </div>
+            <HomeTabsSection />
           </div>
         </section>
 

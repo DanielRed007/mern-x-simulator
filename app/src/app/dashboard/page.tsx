2cb7831f5977216b53099f17b5fc7f359588f6e5
danielred007 <danielrojo1927@gmail.com>
2024-07-04 22:15:40 +0100

    adding login and register refactor, and alert custom component

--- app/src/app/dashboard/page.tsx
+++ app/src/app/dashboard/page.tsx
@@ -1,9 +1,38 @@
 "use client";
 
+import { Fragment, useEffect, useState } from "react";
 import withAuth from "../components/auth/Authorized";
+import Alert from "../components/shared/Alert";
 
-function Page() {
-  return <main className="">This is my dashboard</main>;
+function Dashboard() {
+  const [showAlert, setShowAlert] = useState(false);
+
+  const handleShowAlert = () => {
+    setShowAlert(true);
+    setTimeout(() => setShowAlert(false), 5000);
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      handleShowAlert();
+    }
+  }, []);
+
+  return (
+    <Fragment>
+      <main className="">This is my dashboard</main>
+
+      {showAlert && (
+        <Alert
+          message="You Are Logged In"
+          duration={3000}
+          onClose={() => setShowAlert(false)}
+        />
+      )}
+    </Fragment>
+  );
 }
 
-export default withAuth(Page);
+export default withAuth(Dashboard);
